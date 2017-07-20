69decca84dfe21fdbb00a029b74a8e9a99677853
Manik Charan <mkchan2951@gmail.com>
2017-07-20 23:35:32 +0530

    Trying new lazy depth allocation

--- src/search.c
+++ src/search.c
@@ -684,7 +684,7 @@ int begin_search(struct SearchUnit* const su)
 					sp_tmp = search_params + i;
 					sp_tmp->alpha = alpha;
 					sp_tmp->beta  = beta;
-					sp_tmp->depth = depth + ((i+1)/2 + 1);
+					sp_tmp->depth = depth + ((i/2 + 1) % 4);
 					pthread_create(search_threads + i, NULL, parallel_search, sp_tmp);
 				}
 			}
